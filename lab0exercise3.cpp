943126c08d7c843e7debc0ce6e2942c734bf60b6
sqalli <mts517@nyu.edu>
2023-08-22 20:07:07 +0400

    Create lab0exercise3.cpp
    Exercise 3 of lab 0 introduces students to arithmetics and loops through the computation of Fibonacci sequence

---
+++ lab0exercise3.cpp
@@ -0,0 +1,22 @@
+/*The factorial of n (written n!) is the product of the integers between 1 and n.
+Thus 4! = 1*2*3*4 = 24. By definition, 0! = 1. Factorial is not defined for 
+negative numbers.
+
+Write a program to find factorial of a number. Implement the C++ code using 
+a loop*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+   //Declare two integers 1. for the results of the factorial of the number and 2. one for number that the user desires to compute
+    cout << "Provide a positive integer to compute its factorial\n";
+    // Get the cin >> num;
+    copy = num; // just for the final print statment
+
+    while(/* Factorial Condition */) // Factorial Computational Update
+    
+    cout << copy << "! = " << fact << '\n'; // Printing the final Result
+    return 0;
+}
