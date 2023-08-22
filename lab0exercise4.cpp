b8bc5af473b7d66395c950ebe2112e757a4c2679
sqalli <mts517@nyu.edu>
2023-08-22 20:13:03 +0400

    Create lab0exercise4.cpp
    Exercise 4 in lab 0 introduces functions in C++ by building up on Exercise 3 to compute Fibonacci number of a non-negative integer

---
+++ lab0exercise4.cpp
@@ -0,0 +1,26 @@
+/*4 - A: Write a function to compute Fibonacci number of a non-negative integer. The 
+mathematical definition of the Fibonacci number is:
+    fib(0) = 0
+    fib(1) = 1
+    fib(n) = fib(n-1) + fib(n-2)
+*/
+
+/*4 - B: Write a function that "returns" more than one value. What the function does
+isn't actually important.*/
+
+#include <iostream>
+
+using namespace std;
+
+
+int fib(u_int num){
+    if(num <= 1) return 1;
+    else return // returns the computation of the Fibonacci number as per the instruction above  fib(n) = fib(n-1) + fib(n-2)
+}
+
+int main(int argc, char *argv[]){
+    u_int num = 5;
+    cout << "fib(" << num << ") = " << fib(num) << '\n';
+
+    return 0;
+}
